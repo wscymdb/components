e4f8d2060d7e9545c26ed1280475e7619c59c9aa
wscymdb <1724215773@qq.com>
2022-12-05 16:14:47 +0800

    fix:修复了如果设置boxsizing的值是border-box导致高度和预期有偏差的bug

--- collapse/collapse.vue
+++ collapse/collapse.vue
@@ -14,76 +14,82 @@
 
 <script>
 export default {
-  name: 'ymcollapse',
+  name: "ymcollapse",
   props: {
-    'btn-text': {
+    "btn-text": {
       type: String,
-      default: '查看详情'
-    }
+      default: "查看详情",
+    },
   },
   mounted() {
-    this.getSlotsContHeight()
+    this.getSlotsContHeight();
   },
   data() {
     return {
       innerHeight: 0,
       slotsHeight: 0,
-      isCollapse: true
-    }
+      isCollapse: true,
+    };
   },
   methods: {
     // 点击事件
     handleClick() {
       if (this.innerHeight) {
-        this.isCollapse = true
-        this.innerHeight = 0
+        this.isCollapse = true;
+        this.innerHeight = 0;
       } else {
-        this.isCollapse = false
-        this.innerHeight = this.slotsHeight + 'px'
+        this.isCollapse = false;
+        this.innerHeight = this.slotsHeight + "px";
       }
 
-      this.$emit('collapse-btn-click', { isCollapse: this.isCollapse })
+      this.$emit("collapse-btn-click", { isCollapse: this.isCollapse });
     },
 
     // 获取inner区域的高度
     getSlotsContHeight() {
-      const defaultSlots = this.$slots.default
+      const defaultSlots = this.$slots.default;
       if (!defaultSlots) {
-        this.slotsHeight = this.getFullHeight(this.$refs.contentRef)
-        return
+        this.slotsHeight = this.getFullHeight(this.$refs.contentRef);
+        return;
       }
 
       this.slotsHeight = defaultSlots.reduce((acc, curr) => {
-        return (acc += this.getFullHeight(curr.elm))
-      }, 0)
+        return (acc += this.getFullHeight(curr.elm));
+      }, 0);
     },
     // 根据元素获取计算之后的高度
     getFullHeight(el) {
-      const computedStyle = window.getComputedStyle(el, null)
-      const marginT = computedStyle['margin-top'].split('px')[0] * 1
-      const marginB = computedStyle['margin-bottom'].split('px')[0] * 1
-      const paddingT = computedStyle['padding-top'].split('px')[0] * 1
-      const paddingB = computedStyle['padding-bottom'].split('px')[0] * 1
-      const heigth = computedStyle['height'].split('px')[0] * 1
+      const computedStyle = window.getComputedStyle(el, null);
+      const isBorderBox = computedStyle["box-sizing"];
+      const marginT = computedStyle["margin-top"].split("px")[0] * 1;
+      const marginB = computedStyle["margin-bottom"].split("px")[0] * 1;
+      const paddingT = computedStyle["padding-top"].split("px")[0] * 1;
+      const paddingB = computedStyle["padding-bottom"].split("px")[0] * 1;
+      const heigth = computedStyle["height"].split("px")[0] * 1;
 
-      const resultHeigth = marginB + marginT + paddingT + paddingB + heigth
-      return isNaN(resultHeigth) ? 30 : resultHeigth
-    }
+      const resultHeigth = marginB + marginT + paddingT + paddingB + heigth;
+
+      // 如果boxsizing是borderbox那么就不用计算padding的大小
+      if (isBorderBox === "border-box")
+        resultHeigth = marginB + marginT + heigth;
+
+      return isNaN(resultHeigth) ? 30 : resultHeigth;
+    },
   },
   watch: {
     isCollapse() {
-      const iconRef = this.$refs.iconRef
-      const btnRef = this.$refs.btnRef
+      const iconRef = this.$refs.iconRef;
+      const btnRef = this.$refs.btnRef;
       if (this.isCollapse) {
-        btnRef.style.color = '#000'
-        iconRef.style.cssText = `transform: none`
+        btnRef.style.color = "#000";
+        iconRef.style.cssText = `transform: none`;
       } else {
-        btnRef.style.color = '#409eff'
-        iconRef.style.cssText = `transform: rotate(180deg);`
+        btnRef.style.color = "#409eff";
+        iconRef.style.cssText = `transform: rotate(180deg);`;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
